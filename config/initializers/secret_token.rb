102e58176e09de29374d4c3dcee1760456ace616
pastyl <pastyloczi@gmail.com>
2012-06-04 19:48:54 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biblia::Application.config.secret_token = 'b2393d59bb7bcc18f72a9797d4e8ca0a7eb15fd552225bd0e899647bb838476e3241abec9db1d199cd78b6473ec65e4be75e9eceddd92fb13f3d8afdbc173ec9'
